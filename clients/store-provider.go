b0456cd373130424966513aac83b8de3fd7d876c
omecodes <omecodes@gmail.com>
2020-12-25 22:36:47 +0100

    Made sure auth metadata are passed when requesting service

--- clients/store-provider.go
+++ clients/store-provider.go
@@ -3,6 +3,7 @@ package clients
 import (
 	"context"
 	"github.com/omecodes/libome/errors"
+	"github.com/omecodes/omestore/auth"
 	"github.com/omecodes/omestore/common"
 	"github.com/omecodes/omestore/pb"
 	"github.com/omecodes/service"
@@ -13,7 +14,7 @@ type StoreProvider struct{}
 func (p *StoreProvider) GetClient(ctx context.Context, serviceType uint32) (pb.HandlerUnitClient, error) {
 	switch serviceType {
 	case common.ServiceTypeObjects:
-		conn, err := service.Connect(ctx, common.ServiceTypeObjects)
+		conn, err := service.Connect(auth.SetMetaWithExisting(ctx), common.ServiceTypeObjects)
 		if err != nil {
 			return nil, err
 		}
