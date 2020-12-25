2c85440fbd14aedbc753567182868df45a8efd33
omecodes <omecodes@gmail.com>
2020-12-25 01:39:18 +0100

    Accepts only service type objects

--- clients/store-provider.go
+++ clients/store-provider.go
@@ -2,6 +2,7 @@ package clients
 
 import (
 	"context"
+	"github.com/omecodes/libome/errors"
 	"github.com/omecodes/omestore/common"
 	"github.com/omecodes/omestore/pb"
 	"github.com/omecodes/service"
@@ -10,9 +11,16 @@ import (
 type StoreProvider struct{}
 
 func (p *StoreProvider) GetClient(ctx context.Context, serviceType uint32) (pb.HandlerUnitClient, error) {
-	conn, err := service.Connect(ctx, common.ServiceTypeObjects)
-	if err != nil {
-		return nil, err
+	switch serviceType {
+	case common.ServiceTypeObjects:
+		conn, err := service.Connect(ctx, common.ServiceTypeObjects)
+		if err != nil {
+			return nil, err
+		}
+		return pb.NewHandlerUnitClient(conn), nil
+
+	default:
+		return nil, errors.New(errors.CodeNotImplemented, "no client for this service type")
 	}
-	return pb.NewHandlerUnitClient(conn), nil
+
 }
