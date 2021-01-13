e26ea9f53ce0a7999062432c30c08d6926983f93
omecodes <omecodes@gmail.com>
2021-01-13 21:38:45 +0100

    Added accounts managements

--- accounts/accounts.go
+++ accounts/accounts.go
@@ -1,5 +1,16 @@
 package accounts
 
+import "context"
+
+type Source struct {
+	Provider  string `json:"provider,omitempty"`
+	Name      string `json:"name,omitempty"`
+	FullName  string `json:"source_name,omitempty"`
+	FirstName string `json:"first_name,omitempty"`
+	LastName  string `json:"last_name,omitempty"`
+	Email     string `json:"email,omitempty"`
+}
+
 type Session struct {
 	Token     string `json:"token,omitempty"`
 	CreatedAt int64  `json:"created_at,omitempty"`
@@ -8,20 +19,16 @@ type Session struct {
 }
 
 type Account struct {
-	Name        string              `json:"source,omitempty"`
-	FullName    string              `json:"source_name,omitempty"`
-	FirstName   string              `json:"first_name,omitempty"`
-	LastName    string              `json:"last_name,omitempty"`
-	Email       string              `json:"email,omitempty"`
 	Login       string              `json:"login,omitempty"`
+	Source      *Source             `json:"source,omitempty"`
 	CreatedAt   int64               `json:"created_at,omitempty"`
 	Sessions    map[string]*Session `json:"sessions,omitempty"`
 	Preferences map[string]string   `json:"preferences,omitempty"`
 }
 
 type Manager interface {
-	Create(account *Account) error
-	Get(username string) (*Account, error)
-	Find(provider string, originalName string) (*Account, error)
-	Search(pattern string) ([]string, error)
+	Create(ctx context.Context, account *Account) error
+	Get(ctx context.Context, username string) (*Account, error)
+	Find(ctx context.Context, provider string, originalName string) (*Account, error)
+	Search(ctx context.Context, pattern string) ([]string, error)
 }
--- accounts/sql-manager.go
+++ accounts/sql-manager.go
@@ -1,26 +1,86 @@
 package accounts
 
-import "database/sql"
+import (
+	"context"
+	"database/sql"
+	"encoding/json"
+	"github.com/omecodes/bome"
+	"github.com/omecodes/common/utils/log"
+	"github.com/omecodes/errors"
+)
 
 func NewSQLManager(db *sql.DB, dialect string, tablePrefix string) (Manager, error) {
 	return &sqlManager{}, nil
 }
 
 type sqlManager struct {
+	accounts bome.Map
+	sources  bome.DoubleMap
 }
 
-func (s *sqlManager) Create(account *Account) error {
+func (s *sqlManager) Create(ctx context.Context, account *Account) error {
+	encoded, err := json.Marshal(account)
+	if err != nil {
+		return err
+	}
+
+	tx, err := s.accounts.BeginTransaction()
+	if err != nil {
+		return err
+	}
+
+	err = tx.Save(&bome.MapEntry{
+		Key:   account.Login,
+		Value: string(encoded),
+	})
+	if err != nil {
+		if rer := tx.Rollback(); rer != nil {
+			log.Error("Transaction rollback failed", log.Err(err))
+		}
+
+		if bome.IsPrimaryKeyConstraintError(err) {
+			return errors.Create(errors.DuplicateResource, "account_exists", errors.Info{
+				Name:    "db",
+				Details: err.Error(),
+			})
+		}
+		return errors.Create(errors.Internal, "could_not_create_account", errors.Info{
+			Name:    "bome.SQL",
+			Details: err.Error(),
+		})
+	}
+
+	stx := s.sources.ContinueTransaction(tx.TX())
+	err = stx.Save(&bome.DoubleMapEntry{
+		FirstKey:  account.Source.Provider,
+		SecondKey: account.Source.Name,
+		Value:     account.Login,
+	})
+	if err != nil {
+		if rer := tx.Rollback(); rer != nil {
+			log.Error("Transaction rollback failed", log.Err(err))
+		}
+
+		if bome.IsPrimaryKeyConstraintError(err) {
+			return errors.Create(errors.DuplicateResource, "account_exists", errors.Info{
+				Name:    "db",
+				Details: err.Error(),
+			})
+		}
+		return err
+	}
+
+	return nil
+}
+
+func (s *sqlManager) Get(ctx context.Context, username string) (*Account, error) {
 	panic("implement me")
 }
 
-func (s *sqlManager) Get(username string) (*Account, error) {
+func (s *sqlManager) Find(ctx context.Context, provider string, originalName string) (*Account, error) {
 	panic("implement me")
 }
 
-func (s *sqlManager) Find(provider string, originalName string) (*Account, error) {
-	panic("implement me")
-}
-
-func (s *sqlManager) Search(pattern string) ([]string, error) {
+func (s *sqlManager) Search(ctx context.Context, pattern string) ([]string, error) {
 	panic("implement me")
 }
