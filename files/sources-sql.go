70a442ac9b5a6268e9252984c002dd3485387294
omecodes <omecodes@gmail.com>
2021-02-15 23:44:00 +0100

    Enhanced source manager: introduced user source mapping

--- files/sources-sql.go
+++ files/sources-sql.go
@@ -7,22 +7,38 @@ import (
 	"database/sql"
 	"encoding/binary"
 	"encoding/json"
+	"fmt"
+	"github.com/omecodes/store/auth"
+	"time"
+
 	"github.com/omecodes/bome"
 	"github.com/omecodes/libome/logs"
-	"time"
 )
 
-func NewSourceSQLManager(db *sql.DB, dialect string, tableName string) (*sourceSQLManager, error) {
-	m, err := bome.Build().
+func NewSourceSQLManager(db *sql.DB, dialect string, tablePrefix string) (*sourceSQLManager, error) {
+	builder := bome.Build().
 		SetConn(db).
-		SetDialect(dialect).
-		SetTableName(tableName).
-		Map()
-	return &sourceSQLManager{bMap: m}, err
+		SetDialect(dialect)
+
+	sources, err := builder.SetTableName(tablePrefix + "_sources").JSONMap()
+	if err != nil {
+		return nil, err
+	}
+
+	userRefs, err := builder.SetTableName(tablePrefix + "_sources_user_refs").DoubleMap()
+	if err != nil {
+		return nil, err
+	}
+
+	return &sourceSQLManager{
+		sources:  sources,
+		userRefs: userRefs,
+	}, err
 }
 
 type sourceSQLManager struct {
-	bMap *bome.Map
+	sources  *bome.JSONMap
+	userRefs *bome.DoubleMap
 }
 
 func (s *sourceSQLManager) generateID() (string, error) {
@@ -36,7 +52,7 @@ func (s *sourceSQLManager) generateID() (string, error) {
 	return string(idBytes), nil
 }
 
-func (s *sourceSQLManager) Save(_ context.Context, source *Source) (string, error) {
+func (s *sourceSQLManager) Save(ctx context.Context, source *Source) (string, error) {
 	var err error
 	if source.ID == "" {
 		source.ID, err = s.generateID()
@@ -50,14 +66,59 @@ func (s *sourceSQLManager) Save(_ context.Context, source *Source) (string, erro
 		return "", err
 	}
 
-	return source.ID, s.bMap.Upsert(&bome.MapEntry{
+	var sources *bome.JSONMap
+	var userRefs *bome.DoubleMap
+
+	ctx, sources, err = s.sources.Transaction(ctx)
+	if err != nil {
+		return "", err
+	}
+
+	err = sources.Upsert(&bome.MapEntry{
 		Key:   source.ID,
 		Value: string(encoded),
 	})
+	if err != nil {
+		_ = sources.Rollback()
+	}
+
+	ctx, userRefs, err = s.userRefs.Transaction(ctx)
+	if err != nil {
+		if rbe := bome.Rollback(ctx); rbe != nil {
+			logs.Error("could not rollback transaction", logs.Err(err))
+		}
+		return "", err
+	}
+
+	if source.PermissionOverrides != nil {
+		creator := source.CreatedBy
+
+		perms := append([]*auth.Permission{}, source.PermissionOverrides.Read...)
+		perms = append([]*auth.Permission{}, source.PermissionOverrides.Write...)
+		perms = append([]*auth.Permission{}, source.PermissionOverrides.Chmod...)
+
+		for _, perm := range perms {
+			for _, user := range perm.RelatedUsers {
+				err = userRefs.Upsert(&bome.DoubleMapEntry{
+					FirstKey:  user,
+					SecondKey: source.ID,
+					Value:     creator,
+				})
+				if err != nil {
+					if rbe := bome.Rollback(ctx); rbe != nil {
+						logs.Error("could not rollback transaction", logs.Err(err))
+					}
+					return "", err
+				}
+			}
+		}
+	}
+
+	return source.ID, bome.Commit(ctx)
 }
 
 func (s *sourceSQLManager) Get(_ context.Context, id string) (*Source, error) {
-	strEncoded, err := s.bMap.Get(id)
+	strEncoded, err := s.sources.Get(id)
 	if err != nil {
 		return nil, err
 	}
@@ -67,15 +128,56 @@ func (s *sourceSQLManager) Get(_ context.Context, id string) (*Source, error) {
 	return source, err
 }
 
-func (s *sourceSQLManager) List(_ context.Context) ([]*Source, error) {
-	cursor, err := s.bMap.List()
+func (s *sourceSQLManager) Delete(ctx context.Context, id string) error {
+	var (
+		sources  *bome.JSONMap
+		userRefs *bome.DoubleMap
+		err      error
+	)
+
+	ctx, sources, err = s.sources.Transaction(ctx)
+	if err != nil {
+		return err
+	}
+
+	err = sources.Delete(id)
+	if err != nil && !bome.IsNotFound(err) {
+		return err
+	}
+
+	ctx, userRefs, err = s.userRefs.Transaction(ctx)
+	if err != nil {
+		if rbe := bome.Rollback(ctx); rbe != nil {
+			logs.Error("could not rollback transaction", logs.Err(err))
+		}
+		return err
+	}
+
+	err = userRefs.DeleteAllMatchingSecondKey(id)
+	if err != nil {
+		if rbe := bome.Rollback(ctx); rbe != nil {
+			logs.Error("could not rollback transaction", logs.Err(err))
+		}
+		return err
+	}
+
+	return bome.Commit(ctx)
+}
+
+func (s *sourceSQLManager) UserSources(ctx context.Context, username string) ([]*Source, error) {
+	query := fmt.Sprintf("select sources.value from %s as sources, %s as refs where sources.name=refs.second_key and refs.first_key=?",
+		s.sources.Table(),
+		s.userRefs.Table(),
+	)
+
+	cursor, err := s.sources.Query(query, bome.StringScanner, username)
 	if err != nil {
 		return nil, err
 	}
 
 	defer func() {
-		if cerr := cursor.Close(); cerr != nil {
-			logs.Error("cursor close", logs.Err(cerr))
+		if clerr := cursor.Close(); clerr != nil {
+			logs.Error("close cursor", logs.Err(clerr))
 		}
 	}()
 
@@ -86,21 +188,12 @@ func (s *sourceSQLManager) List(_ context.Context) ([]*Source, error) {
 			return nil, err
 		}
 
-		entry := o.(*bome.MapEntry)
 		var source *Source
-		err = json.Unmarshal([]byte(entry.Value), &source)
+		err = json.Unmarshal([]byte(o.(string)), &source)
 		if err != nil {
 			return nil, err
 		}
 		sources = append(sources, source)
 	}
 	return sources, nil
-}
-
-func (s *sourceSQLManager) Delete(_ context.Context, id string) error {
-	return s.bMap.Delete(id)
-}
-
-func (s *sourceSQLManager) UserSources(ctx context.Context, username string) ([]*Source, error) {
-	return nil, nil
 }
