9cd6075d57c2146fa1d4650c6449473e50a4e7cd
omecodes <omecodes@gmail.com>
2021-05-03 18:39:43 +0000

    Added methods to add/retrieve router and router provider from context

--- acl/context.go
+++ acl/context.go
@@ -1,11 +1,15 @@
 package acl
 
-import "context"
+import (
+	"context"
+	ome "github.com/omecodes/libome"
+)
 
 type ctxTupleStore struct{}
 type ctxManager struct{}
 type ctxNamespaceConfigStore struct{}
 type ctxCommitTime struct{}
+type ctxRouterProvider struct{}
 
 func getTupleStore(ctx context.Context) TupleStore {
 	o := ctx.Value(ctxTupleStore{})
@@ -41,4 +45,26 @@ func getCommitTime(ctx context.Context) int64 {
 
 func ContextWithManager(parent context.Context, man Manager) context.Context {
 	return context.WithValue(parent, ctxManager{}, man)
+}
+
+// WithRouterContextUpdaterFunc return a function that updates context with router provider
+func WithRouterContextUpdaterFunc(provider RouterProvider) ome.GrpcContextUpdater {
+	return ome.GrpcContextUpdaterFunc(func(ctx context.Context) (context.Context, error) {
+		return context.WithValue(ctx, ctxRouterProvider{}, provider), nil
+	})
+}
+
+// ContextWithRouterProvider creates a new context that contains ctx values in addition of the passed router provider
+func ContextWithRouterProvider(ctx context.Context, p RouterProvider) context.Context {
+	return context.WithValue(ctx, ctxRouterProvider{}, p)
+}
+
+func GetHandler(ctx context.Context, opt ...RouteOption) Handler {
+	o := ctx.Value(ctxRouterProvider{})
+	if o != nil {
+		p := o.(RouterProvider)
+		router := p.GetRouter(ctx)
+		return router.GetHandler(opt...)
+	}
+	return DefaultRouter().GetHandler(opt...)
 }
