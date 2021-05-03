9ae9b5df82eb34d951dcdb9c83d4e9e301ccc0d9
omecodes <omecodes@gmail.com>
2021-05-03 18:36:08 +0000

    Defined handler

---
+++ acl/handler.go
@@ -0,0 +1,19 @@
+package acl
+
+import (
+	"context"
+	pb "github.com/omecodes/store/gen/go/proto"
+)
+
+type Handler interface {
+	SaveNamespaceConfig(ctx context.Context, cfg *pb.NamespaceConfig, opts SaveNamespaceConfigOptions) error
+	GetNamespaceConfig(ctx context.Context, namespaceID string, opts SaveNamespaceOptions) (*pb.NamespaceConfig, error)
+	DeleteNamespaceConfig(ctx context.Context, namespaceID string, opts DeleteNamespaceOptions) error
+
+	SaveACL(ctx context.Context, a *pb.ACL, opts SaveACLOptions) error
+	DeleteACL(ctx context.Context, a *pb.ACL, opts DeleteACLOptions) error
+	CheckACL(ctx context.Context, subjectName string, set *pb.SubjectSet, opts CheckACLOptions) (bool, error)
+
+	GetSubjectSet(ctx context.Context, set *pb.SubjectSet, opts GetSubjectSetOptions) ([]string, error)
+	GetObjectSet(ctx context.Context, set *pb.ObjectSet, opts GetObjectSetOptions) ([]string, error)
+}
