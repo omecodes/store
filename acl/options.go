62e04289758ee91c1021f8cdbc604f475334b3a8
omecodes <omecodes@gmail.com>
2021-05-03 18:38:40 +0000

    Introduced router and routing helpers

---
+++ acl/options.go
@@ -0,0 +1,11 @@
+package acl
+
+type SaveNamespaceConfigOptions struct{}
+type SaveACLOptions struct{}
+type DeleteACLOptions struct{}
+type CheckACLOptions struct{}
+type SaveNamespaceOptions struct{}
+type GetNamespaceOptions struct{}
+type DeleteNamespaceOptions struct{}
+type GetSubjectSetOptions struct{}
+type GetObjectSetOptions struct{}
---
+++ acl/route-opts.go
@@ -0,0 +1,27 @@
+package acl
+
+type routesOptions struct {
+	skipPolicies   bool
+	skipParams     bool
+	skipEncryption bool
+}
+
+type RouteOption func(*routesOptions)
+
+func SkipParamsCheck() RouteOption {
+	return func(r *routesOptions) {
+		r.skipParams = true
+	}
+}
+
+func SkipPoliciesCheck() RouteOption {
+	return func(r *routesOptions) {
+		r.skipPolicies = true
+	}
+}
+
+func SkipEncryption() RouteOption {
+	return func(r *routesOptions) {
+		r.skipEncryption = true
+	}
+}
