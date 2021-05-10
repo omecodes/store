7ddbbf4c48d7022a69bde7ed61687629a913427f
omecodes <omecodes@gmail.com>
2021-05-10 10:54:41 +0000

    Code cleaning

--- acl/handler-exec.go
+++ acl/handler-exec.go
@@ -16,7 +16,7 @@ func (e *ExecHandler) SaveNamespaceConfig(ctx context.Context, cfg *pb.Namespace
 	return man.SaveNamespaceConfig(ctx, cfg)
 }
 
-func (e *ExecHandler) GetNamespaceConfig(ctx context.Context, namespaceID string, opts GetNamespaceOptions) (*pb.NamespaceConfig, error) {
+func (e *ExecHandler) GetNamespaceConfig(ctx context.Context, namespaceID string, _ GetNamespaceOptions) (*pb.NamespaceConfig, error) {
 	man := GetManager(ctx)
 	if man == nil {
 		return nil, errors.Internal("acl.GetNamespaceConfig: could not load acl manager from context")
@@ -24,7 +24,7 @@ func (e *ExecHandler) GetNamespaceConfig(ctx context.Context, namespaceID string
 	return man.GetNamespaceConfig(ctx, namespaceID)
 }
 
-func (e *ExecHandler) DeleteNamespaceConfig(ctx context.Context, namespaceID string, opts DeleteNamespaceOptions) error {
+func (e *ExecHandler) DeleteNamespaceConfig(ctx context.Context, namespaceID string, _ DeleteNamespaceOptions) error {
 	man := GetManager(ctx)
 	if man == nil {
 		return errors.Internal("acl.DeleteNamespaceConfig: could not load acl manager from context")
@@ -32,7 +32,7 @@ func (e *ExecHandler) DeleteNamespaceConfig(ctx context.Context, namespaceID str
 	return man.DeleteNamespaceConfig(ctx, namespaceID)
 }
 
-func (e *ExecHandler) SaveACL(ctx context.Context, a *pb.ACL, opts SaveACLOptions) error {
+func (e *ExecHandler) SaveACL(ctx context.Context, a *pb.ACL, _ SaveACLOptions) error {
 	man := GetManager(ctx)
 	if man == nil {
 		return errors.Internal("acl.SaveACL: could not load acl manager from context")
@@ -40,7 +40,7 @@ func (e *ExecHandler) SaveACL(ctx context.Context, a *pb.ACL, opts SaveACLOption
 	return man.SaveACL(ctx, a)
 }
 
-func (e *ExecHandler) DeleteACL(ctx context.Context, a *pb.ACL, opts DeleteACLOptions) error {
+func (e *ExecHandler) DeleteACL(ctx context.Context, a *pb.ACL, _ DeleteACLOptions) error {
 	man := GetManager(ctx)
 	if man == nil {
 		return errors.Internal("acl.DeleteACL: could not load acl manager from context")
@@ -48,7 +48,7 @@ func (e *ExecHandler) DeleteACL(ctx context.Context, a *pb.ACL, opts DeleteACLOp
 	return man.DeleteACL(ctx, a)
 }
 
-func (e *ExecHandler) CheckACL(ctx context.Context, subjectName string, set *pb.SubjectSet, opts CheckACLOptions) (bool, error) {
+func (e *ExecHandler) CheckACL(ctx context.Context, subjectName string, set *pb.SubjectSet, _ CheckACLOptions) (bool, error) {
 	man := GetManager(ctx)
 	if man == nil {
 		return false, errors.Internal("acl.CheckACL: could not load acl manager from context")
@@ -56,7 +56,7 @@ func (e *ExecHandler) CheckACL(ctx context.Context, subjectName string, set *pb.
 	return man.CheckACL(ctx, subjectName, set)
 }
 
-func (e *ExecHandler) GetSubjectNames(ctx context.Context, set *pb.SubjectSet, opts GetSubjectsNamesOptions) ([]string, error) {
+func (e *ExecHandler) GetSubjectNames(ctx context.Context, set *pb.SubjectSet, _ GetSubjectsNamesOptions) ([]string, error) {
 	man := GetManager(ctx)
 	if man == nil {
 		return nil, errors.Internal("acl.GetSubjectsNames: could not load acl manager from context")
@@ -64,7 +64,7 @@ func (e *ExecHandler) GetSubjectNames(ctx context.Context, set *pb.SubjectSet, o
 	return man.GetSubjectsNames(ctx, set)
 }
 
-func (e *ExecHandler) GetObjectNames(ctx context.Context, set *pb.ObjectSet, opts GetObjectsSetOptions) ([]string, error) {
+func (e *ExecHandler) GetObjectNames(ctx context.Context, set *pb.ObjectSet, _ GetObjectsSetOptions) ([]string, error) {
 	man := GetManager(ctx)
 	if man == nil {
 		return nil, errors.Internal("acl.GetObjectsNames: could not load acl manager from context")
